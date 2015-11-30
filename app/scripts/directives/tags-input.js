0ce34f2495d1795f1b35a8ce50d47e75e6019ba9
lejtmany <lejtmany@gmail.com>
2015-11-30 14:23:41 -0500

    tags-input directive now takes ng-model attribute. Also changed to a select tag  instead of input tag.

--- app/scripts/directives/tags-input.js
+++ app/scripts/directives/tags-input.js
@@ -9,8 +9,11 @@
 angular.module('zuxAdminApp')
   .directive('tagsInput', function () {
     return {
-      template: '<div class="tagsinput-primary"><input class="tagsinput" type="tagsinput" value="Taking walks" name="tagsinput"></div>',
+      template: '<div class="tagsinput-primary"><select class="tagsinput" type="tagsinput" value="Taking walks" name="tagsinput" ng-model="ngModel" multiple></div>',
       restrict: 'E',
+      scope:{
+        ngModel: '='
+      },      
       link: function postLink(scope, element, attrs) {
         element.find(".tagsinput").tagsinput();
       }
